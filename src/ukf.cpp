ac711973e2ddfd48a06bc9db1ee6b67c7570bbde
Kiran Hegde <kiran.hegde121@gmail.com>
2017-08-05 13:13:54 +0530

    CalculateRMSE, ProcessMeasurement and Prediction functions written but not tested

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -24,10 +24,10 @@ UKF::UKF() {
   P_ = MatrixXd(5, 5);
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 30;
+  std_a_ = 1;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 30;
+  std_yawdd_ = 0.5;
 
   // Laser measurement noise standard deviation position1 in m
   std_laspx_ = 0.15;
@@ -51,6 +51,35 @@ UKF::UKF() {
 
   Hint: one or more values initialized above might be wildly off...
   */
+
+  // Time when the state was initialized
+  time_us_ = 0.0;
+  
+  // State vector shape
+  n_x_ = 5;
+
+  // Augmented state vector shape
+  n_aug_ = 7;
+
+  // Sigma point spreading parameter
+  lambda_ = 3 - n_aug_;
+
+  // Predicted sigma points
+  Xsig_pred_ = MatrixXd(n_aug_, 2 * n_aug_ + 1);
+
+  // weights
+  weights_ = VectorXd(2 * n_aug_ + 1);
+  // set weights
+  double weight_0 = lambda_/(lambda_+n_aug_);
+  weights_(0) = weight_0;
+  //2n+1 weights
+  for (int i=1; i<2*n_aug_+1; i++) {  
+    double weight = 0.5/(n_aug_+lambda_);
+    weights_(i) = weight;
+  }
+
+  // is the model initialized?
+  is_initialized_ = false;
 }
 
 UKF::~UKF() {}
@@ -66,6 +95,62 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
   Complete this function! Make sure you switch between lidar and radar
   measurements.
   */
+  if (!is_initialized_){
+    if (meas_package.sensor_type_ == MeasurementPackage::RADAR){
+      //Convert radar from polar to cartesian coordinates and initialize state.
+      float ro = measurement_pack.raw_measurements_(0);
+      float theta = measurement_pack.raw_measurements_(1);
+
+      float px = ro * cos(theta);
+      float py = ro * sin(theta);
+
+      x_(0) = px;
+      x_(1) = py;
+    } 
+    else if (meas_package.sensor_type_ == MeasurementPackage::LIDAR){
+      x_(0) = meas_package.raw_measurements_(0);
+      x_(1) = meas_package.raw_measurements_(1);
+    }
+
+    // Initialize other state variables to zero
+    x_(2) = 0;
+    x_(3) = 0;
+    x_(4) = 0;
+
+    // Initialize object covariance matrix
+    // we are only certain about the px and py variables
+    P << 1, 0, 0, 0, 0,
+        0, 1, 0, 0, 0,
+        0, 0, 100, 0, 0,
+        0, 0, 0, 100, 0, 
+        0, 0, 0, 0, 100;
+
+    // Done initialising state vector and convariance matrix
+    // No need to predict or update
+    is_initialized_ = true;
+
+    // Logging in the time when state was initialized
+    time_us_ = meas_package.timestamp_;
+  }
+
+  else {
+    // Predict and Update the state
+
+    // Time between this and the previous measurement (in seconds)
+    double delta_t = (meas_package.timestamp_ - time_us_) / 1000000.0;
+    time_us_ = meas_package.timestamp_;
+
+    // Predict
+    Prediction(delta_t);
+
+    // Update
+    if (meas_package.sensor_type_ == MeasurementPackage::RADAR){
+      UpdateRadar(meas_package);
+    }
+    else if(meas_package.sensor_type_ == MeasurementPackage::LIDAR){
+      UpdateLidar(meas_package);
+    }
+  }
 }
 
 /**
@@ -80,6 +165,98 @@ void UKF::Prediction(double delta_t) {
   Complete this function! Estimate the object's location. Modify the state
   vector, x_. Predict sigma points, the state, and the state covariance matrix.
   */
+
+  // Generating sigma points to be used by the state transition function
+
+  // Augmented state vector
+  VectorXd x_aug = VectorXd(n_aug_);
+  x_aug.head(n_x_) = x_;
+  x_aug(5) = 0;
+  x_aug(6) = 0;
+
+  // Augmented state covariance matrix
+  MatrixXd P_aug = MatrixXd(n_aug_, n_aug_);
+  P_aug.fill(0.0);
+  P_aug.topLeftCorner(n_x_, n_x_) = P_;
+  P_aug(5, 5) = std_a_ * std_a_;
+  P_aug(6, 6) = std_yawdd_ * std_yawdd_;
+
+  // Sigma point matrix
+  MatrixXd Xsig_aug = MatrixXd(n_aug_, 2 * n_aug_ + 1);
+
+  // calculate square root of P
+  MatrixXd L = P_aug.llt().matrixL();
+
+  //create augmented sigma points
+  Xsig_aug.col(0)  = x_aug;
+  for (int i = 0; i< n_aug_; i++){
+    Xsig_aug.col(i+1)       = x_aug + sqrt(lambda_+n_aug_) * L.col(i);
+    Xsig_aug.col(i+1+n_aug) = x_aug - sqrt(lambda_+n_aug_) * L.col(i);
+  }
+
+  // Predict the sigma points after state transition (after time delta_t)
+  for (int i = 0; i< 2 * n_aug_ + 1; i++){
+    //extract values for better readability
+    double p_x = Xsig_aug(0,i);
+    double p_y = Xsig_aug(1,i);
+    double v = Xsig_aug(2,i);
+    double yaw = Xsig_aug(3,i);
+    double yawd = Xsig_aug(4,i);
+    double nu_a = Xsig_aug(5,i);
+    double nu_yawdd = Xsig_aug(6,i);
+
+    //predicted state values
+    double px_p, py_p;
+
+    //avoid division by zero
+    if (fabs(yawd) > 0.001) {
+        px_p = p_x + v/yawd * ( sin(yaw + yawd*delta_t) - sin(yaw));
+        py_p = p_y + v/yawd * ( cos(yaw) - cos(yaw+yawd*delta_t));
+    }
+    else {
+        px_p = p_x + v*delta_t*cos(yaw);
+        py_p = p_y + v*delta_t*sin(yaw);
+    }
+
+    double v_p = v;
+    double yaw_p = yaw + yawd*delta_t;
+    double yawd_p = yawd;
+
+    //add noise
+    px_p = px_p + 0.5*nu_a*delta_t*delta_t * cos(yaw);
+    py_p = py_p + 0.5*nu_a*delta_t*delta_t * sin(yaw);
+    v_p = v_p + nu_a*delta_t;
+
+    yaw_p = yaw_p + 0.5*nu_yawdd*delta_t*delta_t;
+    yawd_p = yawd_p + nu_yawdd*delta_t;
+
+    //write predicted sigma point into right column
+    Xsig_pred_(0,i) = px_p;
+    Xsig_pred_(1,i) = py_p;
+    Xsig_pred_(2,i) = v_p;
+    Xsig_pred_(3,i) = yaw_p;
+    Xsig_pred_(4,i) = yawd_p;
+  }
+
+  //predicted state mean
+  x_.fill(0.0);
+  //iterate over sigma points
+  for (int i = 0; i < 2 * n_aug_ + 1; i++) {  
+    x_ = x_ + weights_(i) * Xsig_pred_.col(i);
+  }
+
+  //predicted state covariance matrix
+  P_.fill(0.0);
+  //iterate over sigma points
+  for (int i = 0; i < 2 * n_aug_ + 1; i++) {  
+    // state difference
+    VectorXd x_diff = Xsig_pred_.col(i) - x_;
+    //angle normalization
+    while (x_diff(3)> M_PI) x_diff(3)-=2.*M_PI;
+    while (x_diff(3)<-M_PI) x_diff(3)+=2.*M_PI;
+
+    P_ = P_ + weights_(i) * x_diff * x_diff.transpose() ;
+  }
 }
 
 /**
